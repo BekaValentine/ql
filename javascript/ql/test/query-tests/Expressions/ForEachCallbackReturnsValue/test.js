8dc65149d04c12aef984ebf67de2f9358dedc9d7
Rebecca Valentine <beka@gothdyke.mom>
2019-03-01 14:55:09 -0800

    adds tests for ForEachCallbackReturnsValue

--- javascript/ql/test/query-tests/Expressions/ForEachCallbackReturnsValue/test.js
+++ javascript/ql/test/query-tests/Expressions/ForEachCallbackReturnsValue/test.js
@@ -11,11 +11,19 @@ function main() {
 // Application w/ directly referenced functions
 
 [].forEach(function (x) { return 1; });
+<<<<<<< HEAD
 //Array.prototype.forEach.apply([], [function (x) { return 1; }]);
 Array.prototype.forEach.call([], function (x) { return 1; });
 
 [].forEach(x => 1);
 //Array.prototype.forEach.apply([], [x => 1]);
+=======
+Array.prototype.forEach.apply([], [function (x) { return 1; }]);
+Array.prototype.forEach.call([], function (x) { return 1; });
+
+[].forEach(x => 1);
+Array.prototype.forEach.apply([], [x => 1]);
+>>>>>>> adds tests for ForEachCallbackReturnsValue
 Array.prototype.forEach.call([], x => 1);
 
 
@@ -23,12 +31,20 @@ Array.prototype.forEach.call([], x => 1);
 
 var f0 = function (x) { return 1; };
 [].forEach(f0);
+<<<<<<< HEAD
 //Array.prototype.forEach.apply([], [f0]);
+=======
+Array.prototype.forEach.apply([], [f0]);
+>>>>>>> adds tests for ForEachCallbackReturnsValue
 Array.prototype.forEach.call([], f0);
 
 var f1 = x => 1;
 [].forEach(f1);
+<<<<<<< HEAD
 //Array.prototype.forEach.apply([], [f1]);
+=======
+Array.prototype.forEach.apply([], [f1]);
+>>>>>>> adds tests for ForEachCallbackReturnsValue
 Array.prototype.forEach.call([], f1);
 
 
@@ -43,11 +59,19 @@ Array.prototype.forEach.call([], f1);
 //Application w/ directly referenced functions
 
 [].forEach(function (x) { return; });
+<<<<<<< HEAD
 //Array.prototype.forEach.apply([], [function (x) { return; }]);
 Array.prototype.forEach.call([], function (x) { return; });
 
 [].forEach(x => { return; });
 //Array.prototype.forEach.apply([], [x => { return; }]);
+=======
+Array.prototype.forEach.apply([], [function (x) { return; }]);
+Array.prototype.forEach.call([], function (x) { return; });
+
+[].forEach(x => { return; });
+Array.prototype.forEach.apply([], [x => { return; }]);
+>>>>>>> adds tests for ForEachCallbackReturnsValue
 Array.prototype.forEach.call([], x => { return; });
 
 
@@ -55,12 +79,20 @@ Array.prototype.forEach.call([], x => { return; });
 
 var f2 = function (x) { return; };
 [].forEach(f2);
+<<<<<<< HEAD
 //Array.prototype.forEach.apply([], [f2]);
+=======
+Array.prototype.forEach.apply([], [f2]);
+>>>>>>> adds tests for ForEachCallbackReturnsValue
 Array.prototype.forEach.call([], f2);
 
 var f3 = x => { return; };
 [].forEach(f3);
+<<<<<<< HEAD
 //Array.prototype.forEach.apply([], [f3]);
+=======
+Array.prototype.forEach.apply([], [f3]);
+>>>>>>> adds tests for ForEachCallbackReturnsValue
 Array.prototype.forEach.call([], f3);
 
 
